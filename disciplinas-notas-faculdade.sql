63f24e75be2526dde3bf7fdcbe6b959adc571399
matheus-debug <matheussantanaengineer@gmail.com>
2020-10-30 21:46:04 -0300

    Consulta de Dados
    Realizando consulta de dados nas tabelas para a confirmação do funcionamento

--- disciplinas-notas-faculdade.sql
+++ disciplinas-notas-faculdade.sql
@@ -89,3 +89,99 @@ values ('Banco de Dados', 80, 'Ementa...', 2, 2)
         ,('Tecnologia Web', 40, 'Ementa...', 1,2)
         ,('Programação Orientada a Objetos', 40, 'Ementa...', 2,3);
 
+insert into tb_grade_aluno (ano, semestre, p1, p2, id_aluno, id_disciplina)
+values (2020, 1, null, null, 4, 5)
+		,(2020, 1, null, null, 4, 2)
+        ,(2020, 1, null, null, 4, 4)
+        ,(2020, 1, null, null, 2, 1)
+        ,(2020, 1, null, null, 2, 3)
+        ,(2020, 1, null, null, 2, 4)
+        ,(2020, 1, null, null, 5, 5)
+        ,(2020, 1, null, null, 5, 4)
+        ,(2020, 1, null, null, 5, 2);
+        
+/*EXIBINDO OS PROFESSORES E SUA TITULAÇÃO*/
+
+select nome
+		,titulacao
+from tb_professor;
+
+/*Exibir as disciplinas de um curso*/
+
+select id
+		,nome
+from tb_disciplina;
+
+/*EXIBINDO AS DISCIPLINAS DE UM CURSO ESPECIFICO*/
+
+select id
+		,nome
+from tb_disciplina
+where id_curso = 2;
+
+/*EXIBINDO DISCIPLINAS COM CARGA IGUAL A 40h*/
+
+select id
+		,nome
+from tb_disciplina
+where carga = 40;
+
+/*EXIBINDO DISCIPLINAS COM CARGA SUPERIOR A 40h*/
+
+select id
+		,nome
+from tb_disciplina
+where carga > 40;
+
+/*EXIBIR PROFESSORES DE UM DETERMINADO CURSO*/
+
+select b.id
+		,b.nome
+from tb_disciplina a inner join tb_professor b on (a.id_professor = b.id)
+where a.id_curso = 2
+group by b.id
+		,b.nome;
+        
+/*EXIBIR ALUNOS DE UMA DISCIPLINA ORDENADO PELO NOME*/
+
+select b.nome
+from tb_grade_aluno a inner join tb_aluno b on (a.id_aluno = b.id)
+where a.id_disciplina = 4
+order by b.nome;
+
+/*EXIBIR TODOS OS ALUNOS COM MEDIA SUPERIOR A 8,5 EM 2020*/
+
+select b.nome
+from tb_grade_aluno a inner join tb_aluno b on (a.id_aluno = b.id)
+where ((a.p1+a.p2)/2) > 8.5
+  and a.ano = 2020
+  and a.semestre = 1;
+
+/*EXIBIR NOME DO PROFESSOR, DISCIPLINA E NOME DOS ALUNOS QUE TIVERAM MEDIA IGUAL A 10 EM 202*/
+
+select c.nome
+      ,b.nome
+from tb_grade_aluno a inner join tb_disciplina b on (a.id_disciplina = b.id)
+                      inner join tb_professor  c on (b.id_professor = c.id)
+where a.p1 = 10
+  and a.p2 = 10
+  and a.ano = 2020
+  and a.semestre = 1;
+  
+/*EXIBIR QUANTIDADE DE DISCIPLINAS ALOCADAS PARA CADA PROFESSOR*/
+
+select b.nome
+      ,count(a.nome)
+from tb_disciplina a inner join tb_professor b on (a.id_professor = b.id)
+group by b.nome;
+
+
+/*EXIBIR QUANTIDADE DE DISCIPLINAS QUE CADA ALUNO ESTA CURSANDO EM  2020*/
+
+select b.nome
+      ,count(c.nome)
+from tb_grade_aluno a inner join tb_aluno      b on (a.id_aluno = b.id)
+                      inner join tb_disciplina c on (a.id_disciplina = c.id)
+where a.ano = 2020
+  and a.semestre = 1
+group by b.nome;
