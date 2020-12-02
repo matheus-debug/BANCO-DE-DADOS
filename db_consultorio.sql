18efa8ea51ed7959cb975dbed0a4a4e2d8bd596b
matheus-debug <matheussantanaengineer@gmail.com>
2020-12-01 23:51:30 -0300

    Ingestão de dados

--- db_consultorio.sql
+++ db_consultorio.sql
@@ -1,5 +1,10 @@
+/*CRIAÇÃO DO BANCO DE DADOS*/
 create database db_consultorio;
+
+/*SELECIONANDO O BANCO DE DADOS*/
 use db_consultorio;
+
+/*CRIAÇÃO DE TABELAS*/
 create table tb_convenio(
 	id			int auto_increment	not null
     ,tipo		varchar(45)			not null
@@ -45,3 +50,57 @@ create table tb_consulta (
     ,foreign key(id_medico_convenio) references tb_medico_convenio(id)
     ,primary key(id)
 );
+
+/*REALIZANDO INGESTÃO DE DADOS*/
+
+insert into tb_convenio (tipo) values
+	('Ambulatorial')
+    ,('Hospitalar')
+    ,('Referencia')
+    ,('Odontologico');
+
+insert into tb_medico(nome,cfm, espec) values
+	('Dr.João','4862','Clinico')
+    ,('Dr.Pedro', '1235', 'Pedriatra')
+    ,('Dra.Katia', '2645','Odonto')
+    ,('Dr.Felipe', '4625','Clinico')
+    ,('Dr.Marcos', '7895', 'Oftalmologia')
+    ,('Dra.Julia', '4627', 'Psiquiatria')
+    ,('Dr.Jose', '7863', 'Cardiologia')
+    ,('Dra.Maria', '3614', 'Cardiologia');
+    
+insert into tb_medico_convenio(tipo, id_medico) values 
+	('Simples', 1)
+    ,('Simples', 2)
+	,('Simples', 3)
+    ,('Simples', 4)
+    ,('Completo', 5)
+    ,('Completo', 6)
+    ,('Completo', 7)
+    ,('Completo', 8);
+
+insert into tb_paciente (nome, cpf, email, telefone, idade, id_convenio) values
+	('Matheus', '305.367.390-80', 'matheus@gmail.com','996531459','12', 1),
+	('Marcos', '735.502.690-99', 'marcos@gmail.com', '996565656','15', 3),
+	('Daniel', '194.252.270-39','daniel@gmail.com', '996652316','32', 2),
+	('Davi', '328.900.500-33', 'davi@gmail.com', '996652357','27', 4),
+	('Caio', '273.797.550-64', 'caio@gmail.com', '996652314','52', 1),
+	('Eder', '147.477.530-60', 'eder@gmail.com', '996652318' ,'18', 3),
+	('Nelson', '718.862.370-85', 'nelson@gmail.com', '996652319' ,'42', 2),
+	('Eduardo', '876.216.590-92', 'eduardo@gmail.com', '996652310','42', 4),
+	('Nicolas', '736.417.260-26', 'nicolas@gmail.com', '996652311','18', 1),
+	('Wesley', '844.097.600-33', 'wesley@gmail.com', '996652312','18', 3);
+    
+insert into tb_consulta (horario, agenda, id_paciente, id_medico_convenio) values
+	('11:10', '2020/11/5', 10, 8)
+	,('12:10', '2020/11/5', 1, 1)
+	,('11:40', '2020/11/5', 9, 7)
+	,('11:30', '2020/11/5', 2, 2)
+	,('10:10', '2020/11/5', 8, 6)
+	,('11:50', '2020/11/5', 3, 3)
+	,('13:00', '2020/11/5', 7, 5)
+	,('9:10', '2020/11/5', 4, 4)
+	,('7:50', '2020/11/5', 6, 3)
+	,('10:30', '2020/11/5', 5, 2);
+
+
